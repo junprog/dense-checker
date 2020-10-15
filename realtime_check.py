3d54135fca5ef5745967339e33067ae2f821093d
Junya Uchida <jyunya.uchida@gmail.com>
2020-10-15 23:14:37 +0900

    カメラ確認済

--- realtime_check.py
+++ realtime_check.py
@@ -9,28 +9,35 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from torchvision import transforms
+import torchvision.transforms.functional as F
 
-mean = np.array([0.485, 0.456, 0.406])
-std = np.array([0.229, 0.224, 0.225])
+#mean = np.array([0.485, 0.456, 0.406])
+#std = np.array([0.229, 0.224, 0.225])
 
-def extracter(img, model, trans): 
+def extracter(img, model, trans):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #model.to(device)
+
     model.eval()
     with torch.no_grad():
-        img = trans(img)
+        img = trans(img).unsqueeze_(0)
+        #img.to('cuda')
+
         out = model(img)
 
     out = out.to('cpu').detach().numpy().copy()
-    out = out*std+mean
+    out = np.squeeze(out)
+    #out = out*std+mean
 
     return out
 
 def dense_checker():
     ##
-    trans = transforms.Compose([transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225]), transforms.ToTensor()])
+    trans = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
 
     model = models.resnet18(pretrained=True)
-    modules = list(model.children()[:-1])
+    modules = list(model.children())[:-2]
     model = nn.Sequential(*modules)
     model.add_module('output_layer', nn.Conv2d(512,1,(3,3),padding=(1,1)))
 
@@ -41,7 +48,7 @@ def dense_checker():
 
 def capture_camera(trans, model, mirror=True):
     # Capture video from camera
-    cap = cv2.VideoCapture(0) # device num
+    cap = cv2.VideoCapture(0+cv2.CAP_DSHOW) # device num
 
     while True:
         ret, frame = cap.read()
@@ -57,7 +64,7 @@ def capture_camera(trans, model, mirror=True):
         rgb_img[:,:,2] = frame[:,:,0]
 
         out = extracter(rgb_img, model, trans)
-
+        out = cv2.resize(out,dsize=(640, 480))
         # plot
         cv2.imshow('camera capture', out)
 
