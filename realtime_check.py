5ce7bae5315f0171a11ab4eb718adbd9fd22a5dc
Junya Uchida <jyunya.uchida@gmail.com>
2020-10-16 01:35:59 +0900

    微修正

--- realtime_check.py
+++ realtime_check.py
@@ -11,44 +11,9 @@ from torchvision import models
 from torchvision import transforms
 import torchvision.transforms.functional as F
 
-#mean = np.array([0.485, 0.456, 0.406])
-#std = np.array([0.229, 0.224, 0.225])
-
-def extracter(img, model, trans):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #model.to(device)
-
-    model.eval()
-    with torch.no_grad():
-        img = trans(img).unsqueeze_(0)
-        #img.to('cuda')
-
-        out = model(img)
-
-    out = out.to('cpu').detach().numpy().copy()
-    out = np.squeeze(out)
-    #out = out*std+mean
-
-    return out
-
-def dense_checker():
-    ##
-    trans = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
-
-    model = models.resnet18(pretrained=True)
-    modules = list(model.children())[:-2]
-    model = nn.Sequential(*modules)
-    model.add_module('output_layer', nn.Conv2d(512,1,(3,3),padding=(1,1)))
-
-    print(model)
-
-    capture_camera(trans, model)
-    ##
-
-def capture_camera(trans, model, mirror=True):
+def capture_camera(trans, model, device, mirror=True):
     # Capture video from camera
-    cap = cv2.VideoCapture(0+cv2.CAP_DSHOW) # device num
+    cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW) # device num
 
     while True:
         ret, frame = cap.read()
@@ -63,7 +28,7 @@ def capture_camera(trans, model, mirror=True):
         rgb_img[:,:,1] = frame[:,:,1] 
         rgb_img[:,:,2] = frame[:,:,0]
 
-        out = extracter(rgb_img, model, trans)
+        out = extracter(rgb_img, model, trans, device)
         out = cv2.resize(out,dsize=(640, 480))
         # plot
         cv2.imshow('camera capture', out)
@@ -74,6 +39,35 @@ def capture_camera(trans, model, mirror=True):
 
     cap.release()
     cv2.destroyAllWindows()
+
+def extracter(img, model, trans, device):
+    model.to(device)
+
+    model.eval()
+    with torch.no_grad():
+        img = trans(img).unsqueeze_(0)
+        img = img.to(device)
+        out = model(img)
+
+    out = out.to('cpu').detach().numpy().copy()
+    out = np.squeeze(out)
+
+    return out
+
+def dense_checker():
+    ##
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    trans = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+
+    model = models.resnet18(pretrained=True)
+    modules = list(model.children())[:-4]
+    model = nn.Sequential(*modules)
+    model.add_module('output_layer', nn.Conv2d(128,1,(3,3),padding=(1,1)))
+
+    print(model)
+
+    capture_camera(trans, model, device)
 
 if __name__ == '__main__':
     dense_checker()
